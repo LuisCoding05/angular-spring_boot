d61b4f41b4f23c9c2b12ea8cffda122eb8bacdd5
LuisCoding05 <162503462+LuisCoding05@users.noreply.github.com.>
2025-07-12 16:52:07 +0200

    pagina home implementada

--- angular/src/app/app.routes.ts
+++ angular/src/app/app.routes.ts
@@ -1,3 +1,20 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './components/home-component/home-component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+    path: '',
+    title: 'Techflix Home Page',
+    component: HomeComponent,
+    },
+    // {
+    // path: 'dashboard',
+    // title: 'Techflix Dashboard',
+    // // component: Home,
+    // },
+    // {
+    // path: 'login',
+    // title: 'Techflix Login',
+    // // component: Home,
+    // }
+];
---
+++ angular/src/app/components/home-component/home-component.ts
@@ -0,0 +1,12 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home-component',
+  imports: [CommonModule],
+  templateUrl: './home-component.html',
+  styleUrl: './home-component.css'
+})
+export class HomeComponent {
+
+}
